672654f5804c1fa6fd11eebdc191b453d278b9de
Seth Lewis <lewiscatenary@gmail.com>
2020-04-26 18:37:06 -0500

    updated test structure

--- hello-world/project/hello.go
+++ hello-world/project/hello.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+func strSliceEmptyOrNil(s []string) bool {
+	if len(s) == 0 || s[0] == "" {
+		return true
+	}
+	return false
+}
+
 // Greeting is a standard Hello, World greeting
-func Greeting() string {
-	greeting := "Hello, World"
+func Greeting(name ...string) string {
+	var greetee string
+	if strSliceEmptyOrNil(name) {
+		greetee = "World"
+	} else {
+		greetee = name[0]
+	}
+
+	greeting := "Hello, " + greetee
 	return greeting
 }
 
--- hello-world/project/hello_test.go
+++ hello-world/project/hello_test.go
@@ -5,10 +5,30 @@ import (
 )
 
 func TestGreeting(t *testing.T) {
-	expected := "Hello, World"
-	actual := Greeting()
 
-	if expected != actual {
-		t.Errorf("expected %q, actual %q", expected, actual)
+	assertCorrectMessage := func(t *testing.T, expected, actual string) {
+		t.Helper()
+		if expected != actual {
+			t.Errorf("expected %q, actual %q", expected, actual)
+		}
 	}
+
+	t.Run("Hello, World with no input", func(t *testing.T) {
+		expected := "Hello, World"
+		actual := Greeting()
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("Hello, World with an empty string name", func(t *testing.T) {
+		expected := "Hello, World"
+		actual := Greeting("")
+		assertCorrectMessage(t, expected, actual)
+	})
+
+	t.Run("Hello, World with a provided name", func(t *testing.T) {
+		expected := "Hello, Seth"
+		actual := Greeting("Seth")
+		assertCorrectMessage(t, expected, actual)
+	})
+
 }
