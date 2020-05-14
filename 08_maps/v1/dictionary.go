6648264740177984b0266644a6b25afd699fd57b
Seth Lewis <lewiscatenary@gmail.com>
2020-05-13 22:14:51 -0500

    some dict-map stuff

--- 08_maps/v1/dictionary.go
+++ 08_maps/v1/dictionary.go
@@ -1,9 +1,24 @@
 package main
 
+import "errors"
+
 // Dictionary store definitions to words
 type Dictionary map[string]string
 
+// ErrNotFound is the error message for missing words in the dictionary
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Search find a word in the dictionary
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// Add adds a key value pair to a dictionary
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
--- 08_maps/v1/dictionary_test.go
+++ 08_maps/v1/dictionary_test.go
@@ -5,16 +5,58 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		actual, _ := dictionary.Search("test")
+		expected := dictionary["test"]
 
-	assertStrings(t, got, want)
+		assertStrings(t, actual, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, actual := dictionary.Search("unknown")
+		expected := ErrNotFound
+
+		assertErrors(t, actual, expected)
+	})
 }
 
-func assertStrings(t *testing.T, got, want string) {
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	t.Run("Add to dictionary", func(t *testing.T) {
+		key := "test"
+		value := "this is just a test"
+
+		dictionary.Add(key, value)
+
+		assertDefinition(t, dictionary, key, value)
+	})
+}
+
+func assertStrings(t *testing.T, actual, expected string) {
 	t.Helper()
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	if actual != expected {
+		t.Errorf("actual %q expected %q", actual, expected)
+	}
+}
+
+func assertErrors(t *testing.T, actual, expected error) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("actual %q expected error %q", actual, expected)
+	}
+}
+
+func assertDefinition(t *testing.T, d Dictionary, key, value string) {
+	t.Helper()
+
+	actual, err := d.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	if actual != value {
+		t.Errorf("actual %q expected %q", actual, value)
 	}
 }
